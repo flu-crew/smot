c3f281c9b44a0a9cb6a4b4e71b2fa58c5cc64cd7
Zebulun Arendsee <arendsee@iastate.edu>
2020-08-09 17:07:17 -0500

    Specify click dependency

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     entry_points={"console_scripts": ["smot=smot.main:main"]},
+    installation_requires={"click"},
     py_modules=["smot"],
     zip_safe=False,
 )
